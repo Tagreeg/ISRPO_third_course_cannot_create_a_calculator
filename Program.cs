a8f3470dd3b619d417e99759cbdac027727ea2d9
307-03 <radinom3@mail.ru>
2022-03-26 10:35:11 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISRPO_third_course_cannot_create_a_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
